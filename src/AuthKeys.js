ec15af0d10f1eee8a40428d71c9ab0af36caa444
Konrad Hyzy <konrad.hyzy@al-enterprise.com>
2019-06-18 10:22:14 +0200

    Remove node modules

--- src/AuthKeys.js
+++
@@ -1,6 +0,0 @@
-const AuthKeysSandbox = {
-    applicationID: "12fe7c908e9511e99c576faddce77f49",
-    applicationSecret: "BRCtvNYsBl7dMkeGVYmTIEDGoYEMFfCqqPbxx0waiwxo8ulaLRy4AOpSsdmcEvdK"
-}
-
-export { AuthKeysSandbox }
